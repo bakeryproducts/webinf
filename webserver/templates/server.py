b9cc0ef31b0a0e75e8acfcb98ebc9379cc5457ad
bakeryproducts <gleb.m.sokolov@gmail.com>
2021-01-27 17:08:37 -0500

    refact

--- webserver/templates/server.py
+++ webserver/templates/server.py
@@ -7,17 +7,29 @@ from PIL import Image
 from flask import Flask, make_response, send_file, jsonify, request
 
 app = Flask(__name__)
+WEB_HOST, WEB_PORT = 'localhost', '9050'
+TILE_HOST, TILE_PORT = 'localhost', '9051'
 
 @app.route('/')
 def index():
-    p = Path('imgs')
-    imgs = [str(i.name) for i in p.glob('*.tif')]
-    href = lambda x: '<a href="http://localhost:9050/' + x + '">'+x+'</a>'
-    s = '<br/>'.join([href(i) for i in imgs])
+    p = Path('imgs').absolute()
+    #WEB_HOST, WEB_PORT = 'localhost', '9050'
+    imgs, exts = [], ['*.tif', '*.tiff']
+    for ext in exts:
+        imgs.extend(p.glob(ext)) 
+
+    s = ''
+    for img in imgs:
+        img_sub_path = str(img.relative_to(p))
+        url = f'<a href="http://{WEB_HOST}:{WEB_PORT}/' + img_sub_path + '">'+img_sub_path+'</a>'
+        s += url
+        s += '<br/>'
+
     return s
 
 @app.route("/<string:filename>")
 def ll(filename):
+    #host, port = 'localhost', '9051'
     s1 = '''
 <!DOCTYPE html>
 <head>
@@ -36,15 +48,13 @@ def ll(filename):
 }
 </style>
 </head>
-
 <body>
 	<div id="map"></div>
 <script>
 window.onload = function () {
-    var map = L.map('map',{crs: L.CRS.Simple});'''
+var map = L.map('map',{crs: L.CRS.Simple});'''
 
-    s2 = '''
-    var layer = L.tileLayer('http://localhost:9051/'''
+    s2 = f"var layer = L.tileLayer('http://{TILE_HOST}:{TILE_PORT}/"
 
     s3 = '''/{z}_{x}_{y}.png', {
         maxZoom: 10,
@@ -54,8 +64,6 @@ window.onload = function () {
     map.setView([0.0,0.0], 6);
 
     layer.on('tileload', function(e) {
-        console.log(e);
-        console.log(e.coords);
         var dd = {'data':"this is a tset"};
         $.ajax({
             type: "POST",
@@ -63,7 +71,9 @@ window.onload = function () {
             data: JSON.stringify(dd),
             contentType: "application/json; charset=utf-8",
             dataType: "json",
-            success: function(data){console.log(data);},
+            success: function(data){
+                //console.log(data);
+                },
             error: function(errMsg) {
                 alert(errMsg);
             }
@@ -74,27 +84,24 @@ window.onload = function () {
 
 '''
 
-    fetch = '''
-    '''
-
     s4 = '''
 </script>
 </body>
 </html>
         '''
 
-    s = s1 +  s2 + filename + s3+s4
+    s = s1 +  s2 + filename + s3 + s4
     return s 
 
 @app.route('/test', methods=['POST'])
 def testfn():    
     data = request.get_json()
     #data = request.form['data']
-    print(data, type(data))
+    #print(data, type(data))
     return jsonify({'thisis ':'answer'})  
 
 
 if __name__ == "__main__":
-    app.run(port=9050)
+    app.run(port=WEB_PORT)
 
 
