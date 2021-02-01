5c63175679e5668e1570e5bd130312778efa1281
sokolov <gleb.m.sokolov@gmail.com>
2021-02-01 19:42:20 +0300

    dirty image overlay

--- infer/server.py
+++ infer/server.py
@@ -1,8 +1,10 @@
 import io
-import requests
 import argparse
 from pathlib import Path
+from PIL import Image
+import base64
 
+import numpy as np
 from flask import Flask, make_response, send_file, jsonify
 
 app = Flask(__name__)
@@ -10,18 +12,25 @@ app = Flask(__name__)
 @app.route("/infer/<string:filename>")
 def infer_processer(filename):
     filename = filename.replace('__', '/')
+    mask = (np.random.random((200,200,3))*255.).astype(np.uint8)
 
-# assume that "app" below is your flask app, and that
-# "Response" is imported from flask.
+    img_bytes = io.BytesIO()
+    im = Image.fromarray(mask)
+    im.convert('RGBA').save(img_bytes, format='PNG')
+    img_bytes.seek(0, 0)
+    b64_img = base64.b64encode(img_bytes.read())
 
-@app.route("/proxy-example")
-def proxy_example():
-    r = requests.get("http://example.com/other-endpoint")
-    return Response(
-        r.text
-        status=r.status_code,
-        content_type=r.headers['content-type'],
-    )
+    #return send_file(img_bytes, mimetype='image/png', as_attachment=False)
+    return jsonify({'mask':b64_img.decode('utf-8')})
+
+#@app.route("/proxy-example")
+#def proxy_example():
+#    r = requests.get("http://example.com/other-endpoint")
+#    return Response(
+#        r.text
+#        status=r.status_code,
+#        content_type=r.headers['content-type'],
+#    )
 
 
 if __name__ == "__main__":
