8d530deca495c4c6ed7905156e9a273e79593d83
Gleb Sokolov <gleb.m.sokolov@gmail.com>
2021-02-01 16:41:09 +0300

    Update map.js

--- webserver/static/js/map.js
+++ webserver/static/js/map.js
@@ -5,18 +5,15 @@ function winload() {
 	var layer = L.tileLayer('http://{{TILE_HOST}}:{{TILE_PORT}}/tile/{{filename}}/{z}_{x}_{y}.png', {
 		maxZoom: 10,
 		maxNativeZoom: 6,
-		maxBounds: [12817, 11172],
 	}).addTo(map);
 
 	map.setView([0.0,0.0], 6);
 	$.ajax({
 		type: "GET",
 		url: '/label/{{filename}}',
-		//data: JSON.stringify(dd),
 		contentType: "application/json; charset=utf-8",
 		dataType: "json",
 		success: function(data){
-			//console.log(data);
 			data.forEach(function(e){
 				new L.Polyline(e, {
 					color: 'red',
